2abac5a648c5ed3d50156f730db55a1c2d66fd72
Job462 <jobmolina81@gmail.com>
2023-12-12 15:57:37 -0400

    ProyectoFinal Job Molina

---
+++ constants/index.js
@@ -0,0 +1,88 @@
+export const exploreWorlds = [
+  {
+    id: 'world-1',
+    imgUrl: '/planet-01.png',
+    title: 'The Hogwarts',
+  },
+  {
+    id: 'world-2',
+    imgUrl: '/planet-02.png',
+    title: 'The Upside Down',
+  },
+  {
+    id: 'world-3',
+    imgUrl: '/planet-03.png',
+    title: 'Kadirojo Permai',
+  },
+  {
+    id: 'world-4',
+    imgUrl: '/planet-04.png',
+    title: 'Paradise Island',
+  },
+  {
+    id: 'world-5',
+    imgUrl: '/planet-05.png',
+    title: 'Hawkins Labs',
+  },
+];
+
+export const startingFeatures = [
+  'Encuentra un mundo que se adapte a ti y en el que quieras entrar',
+  'Entra al mundo leyendo basmalah para estar seguro',
+  'No hay necesidad de andarse con rodeos, solo manténgase en el acelerador y diviértase',
+];
+
+export const newFeatures = [
+  {
+    imgUrl: '/vrpano.svg',
+    title: 'Un Nuevo Mundo',
+    subtitle:
+        'Tenemos la última actualización con un mundo nuevo para que pruebes, no importa.',
+  },
+  {
+    imgUrl: '/headset.svg',
+    title: 'Más realista',
+    subtitle:
+        'En la última actualización, tus ojos se estrechan, lo que hace que el mundo sea más realista que nunca.',
+  },
+];
+
+export const insights = [
+  {
+    imgUrl: '/planet-06.png',
+    title: 'El lanzamiento del Metaverso hace que ...',
+    subtitle:
+        'Un elemento importante del tiempo del tratamiento médico del paciente. Pero por esa razón me gustaría dibujar una caricatura en otro lugar.',
+  },
+  {
+    imgUrl: '/planet-07.png',
+    title: '7 consejos para dominar fácilmente la locura del Metaverso',
+    subtitle:
+        'Los deberes de la vida son el fútbol y el fútbol hasta. Y de las montañas nacerá un ratón ridículo. A veces la tierra se derrumba',
+  },
+  {
+    imgUrl: '/planet-08.png',
+    title: 'Con una plataforma puedes explorar el mundo entero virtualmente',
+    subtitle:
+        '¿Cuántas personas hacen eso o qué elemento? Viverra gratis, solo laoreet debería ser el curso. Maurice en una ensalada',
+  },
+];
+
+export const socials = [
+  {
+    name: 'twitter',
+    url: '/twitter.svg',
+  },
+  {
+    name: 'linkedin',
+    url: '/linkedin.svg',
+  },
+  {
+    name: 'instagram',
+    url: '/instagram.svg',
+  },
+  {
+    name: 'facebook',
+    url: '/facebook.svg',
+  },
+];
